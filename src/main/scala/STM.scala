3adb9cd3afdab120657e6c270274202c4126ddcc
fabio-labella <fabio.labella2@gmail.com>
2018-07-30 16:48:16 +0200

    Add basic (unsafe) read and write ops

--- src/main/scala/STM.scala
+++ src/main/scala/STM.scala
@@ -4,12 +4,24 @@ object Y {
   import cats._, implicits._
   import cats.free._
   import cats.effect._, concurrent._
+  import scala.collection.mutable
 
   trait STM_[A]
-  case class ReadTVar[A](t: TVar[A]) extends STM_[A]
   case class NewTVar[A](a: A) extends STM_[TVar[A]]
+  case class ReadTVar[A](t: TVar[A]) extends STM_[A]
+  case class WriteTVar[A](t: TVar[A], v: A) extends STM_[Unit]
 
   type STM[A] = Free[STM_, A]
+
+  def newTVar[A](a: A): STM[TVar[A]] =
+   Free.liftF[STM_, TVar[A]](NewTVar(a))
+
+  def readTVar[A](t: TVar[A]): STM[A] =
+    Free.liftF(ReadTVar(t))
+
+  def writeTVar[A](t: TVar[A], a: A): STM[Unit] =
+    Free.liftF[STM_, Unit](WriteTVar(t, a))
+
 
   /** Represents a unique identifier (using object equality). */
   private[stm] final class Token extends Serializable {
@@ -20,59 +32,52 @@ object Y {
     var a: A,
     val id: Token
   )
-  object TVar {
-    def create[A](a: A): IO[TVar[A]] = IO {
-      new TVar(a, new Token)
+
+  private[stm] final class Entry[A](val tvar: TVar[A], val initial: A, var current: A)
+  private[stm] final class TRec(val entries: mutable.Map[Token, Entry[Any]]) {
+    def insert[A](tvar: TVar[A], v: A): Unit = {
+      entries += tvar.id -> new Entry(tvar.asInstanceOf[TVar[Any]], v, v)
+      ()
     }
-      
+
   }
 
-  // todo is hashcode influenced by mutable field?
-  case class TRec(
-    vars: Map[Token, TVar[_]],
-    values: Map[Token, Any]
-  )
-
-  def newTVar[A](a: A): STM[TVar[A]] =
-   Free.liftF[STM_, TVar[A]](NewTVar(a))
-
-  def readTVar[A](t: TVar[A]): STM[A] =
-    Free.liftF(ReadTVar(t))
-
-  def interpreter(state: Ref[IO, TRec]): STM_ ~> IO = {
-    def nt =  new (STM_ ~> IO) {
-      def apply[A](prog: STM_[A]): IO[A] = prog match {
-        case NewTVar(a) => TVar.create(a)
-        case ReadTVar(tvar) => state.modify(readTVarImpl(tvar))
-      }
-    }
-
-    def readTVarImpl[A](tvar: TVar[A]): TRec => (TRec, A) =
-      trec => {
-        // todo access to tvar fields is not in IO, fix?
-        trec.values.get(tvar.id) match {
-          case Some(v) => trec -> v.asInstanceOf[A]
-          case None =>
-            val v = tvar.a
-            val id = tvar.id
-            trec.copy(
-              vars = trec.vars + (id -> tvar),
-              values = trec.values + (id -> v)
-            ) -> v.asInstanceOf[A]
-          
+  def interpreter: IO[STM_ ~> IO] =
+    IO(new TRec(mutable.Map.empty)).map { trec =>
+      new (STM_ ~> IO) {
+        def apply[A](prog: STM_[A]): IO[A] = prog match {
+          case NewTVar(a) => IO(new TVar(a, new Token))
+          case ReadTVar(tvar) => IO {
+            trec.entries.get(tvar.id) match {
+              case Some(v) => v.current.asInstanceOf[A]
+              case None =>
+                val v = tvar.a
+                trec.insert(tvar, v)
+                v
+            }
+          }
+          case WriteTVar(tvar, v) => IO {
+            trec.entries.get(tvar.id) match {
+              case Some(entry) => entry.current = v
+              case None => trec.insert(tvar, v)
+            }
+          }
         }
       }
+    }
 
-    nt
-  }
+  def atomically[A]: STM[A] => IO[A] = p =>
+    interpreter.flatMap(p.foldMap(_))
 
 
   def yo = for {
     v <- newTVar(1)
     a <- readTVar(v)
+    _ <- writeTVar(v, 2)
     b <- readTVar(v)
-  } yield a + b
+    c = v.a
+  } yield (a, b, c)
 
-  def p = yo.foldMap(interpreter(Ref.unsafe[IO, TRec](TRec(Map.empty, Map.empty)))).unsafeRunSync
+  def p = atomically(yo).unsafeRunSync
 
 }
